6be9268cdf49daaecf9312145923098f967908cb
3mmar 3li <ammaralaa572@gmail.com>
2026-02-19 17:08:08 +0200

    finish imagekit webhook & try to solve unhandled revieing events

--- src/config/imagekit.ts
+++ src/config/imagekit.ts
@@ -2,9 +2,9 @@ import ImageKit from "imagekit";
 import AppError from "../utils/AppError";
 
 if (
-  !process.env.IMGAGE_KIT_PUBLIC_KEY ||
-  !process.env.IMGAGE_KIT_PUBLIC_KEY ||
-  !process.env.IMGAGE_KIT_URL_ENDPOINT
+  !process.env.IMAGE_KIT_PUBLIC_KEY ||
+  !process.env.IMAGE_KIT_PRIVATE_KEY ||
+  !process.env.IMAGE_KIT_URL_ENDPOINT
 ) {
   throw new AppError(
     "Missing important env variables related to image kit",
@@ -12,9 +12,9 @@ if (
   );
 }
 const imagekit = new ImageKit({
-  publicKey: process.env.IMGAGE_KIT_PUBLIC_KEY!,
-  privateKey: process.env.IMGAGE_KIT_PUBLIC_KEY!,
-  urlEndpoint: process.env.IMGAGE_KIT_URL_ENDPOINT!,
+  publicKey: process.env.IMAGE_KIT_PUBLIC_KEY!,
+  privateKey: process.env.IMAGE_KIT_PRIVATE_KEY!,
+  urlEndpoint: process.env.IMAGE_KIT_URL_ENDPOINT!,
 });
 
 export default imagekit;
--- src/controllers/imageKitWebhookController.ts
+++ src/controllers/imageKitWebhookController.ts
@@ -2,5 +2,9 @@ import catchAsync from "../utils/catchAsync";
 
 
 export const imageKitWebhook = catchAsync(async (req, res, next) => {
-  
+  console.log("Webhook imageKit fired here =>", req.body);
+  res.json({
+    msg: "Webhook recived",
+    data: req.body,
+  })
 })
