62c4a4af177abc2ee0c9e49ac8060596ecaaed23
3mmar 3li <ammaralaa572@gmail.com>
2026-02-23 16:19:38 +0200

    added protec to handle user id

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,11 +1,13 @@
 import express from "express"
 import { signin, signup, updatePassword } from "../controllers/authController";
+import { protect } from "../middleware/authMiddleware";
 
 const router = express.Router();
 
 router.post("/sign-up", signup);
 router.post("/login", signin);
-router.patch("/update-password", updatePassword);
+
+router.patch("/update-password",protect, updatePassword);
 
 
 export default router;
